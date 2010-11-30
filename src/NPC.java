da62195c6f19e308243b06452393462f1223d785
XAMPP <windowsxnot@hotmail.com>
2010-11-29 19:22:45 -0600

    started on the npc object next is the prop mananager

--- src/NPC.java
+++ src/NPC.java
@@ -8,5 +8,71 @@
  * @author vijay
  */
 public class NPC {
+    public fx usr; // fx == user object
+    public NPC(String name, float x,float y, float z, float rot, float pitch){
 
+    }
+     public NPC(String name, float x,float y, float z, float rot, float pitch, int itm){
+
+    }
+     public void Hide(){
+
+     }
+     public void Delete(){
+
+     }
+     public void Track(){
+
+     }
+     public void Untrack(){
+
+     }
+     public void BrodcastPos(){
+
+     }
+     public void BrodcastMov(){
+
+     }
+     public String GetName(){
+
+     }
+     public String SetName(){
+
+     }
+     public Location GetLocation(){
+
+
+     }
+     public void SetLocation(Location loc){
+
+     }
+     public float GetRot(){
+
+     }
+     public void SetRot(float rot){
+
+     }
+     public float GetPitch(){
+
+     }
+     public void SetPitch(float pitch){
+
+     }
+     public void SetPitchAndRot(float rot,float pitch){
+
+     }
+     public int GetItmInHand(){
+
+     }
+     public void SetItmInHand(int itm){
+
+    }
+     public void TpTo(double x ,double y,double z,float rot,float pitch){
+        Location loc = new Location();
+        loc.x = x;
+        loc.y = y;
+        loc.z = z;
+        SetLocation(loc);
+        SetPitchAndRot(rot,pitch);
+     }
 }
