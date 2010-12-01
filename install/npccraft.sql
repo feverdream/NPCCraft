a072a39b6bba7fc1b8de4bbcec4959d9c6e427ce
XAMPP <windowsxnot@hotmail.com>
2010-12-01 01:29:30 -0600

    More work on more things Need to get evrything BUT NPC class to work so i can the Get it to work and be abel to handel updates better

--- install/npccraft.sql
+++ install/npccraft.sql
@@ -4,7 +4,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 29, 2010 at 12:11 AM
+-- Generation Time: Nov 30, 2010 at 11:41 PM
 -- Server version: 5.1.49
 -- PHP Version: 5.3.3-1ubuntu9.1
 
@@ -29,36 +29,39 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 CREATE TABLE IF NOT EXISTS `npccraft` (
   `npcid` int(11) NOT NULL AUTO_INCREMENT,
   `npcname` varchar(15) NOT NULL,
-  `npclast_x` float NOT NULL,
-  `npclast_y` float NOT NULL,
-  `npclast_z` float NOT NULL,
-  `npcbound_1_x` float NOT NULL,
-  `npcbound_1_y` float NOT NULL,
-  `npcbound_1_z` float NOT NULL,
-  `npcbound_2_x` float NOT NULL,
-  `npcbound_2_y` float NOT NULL,
-  `npcbound_2_z` float NOT NULL,
-  `npcbound_3_x` float NOT NULL,
-  `npcbound_3_y` float NOT NULL,
-  `npcbound_3_z` float NOT NULL,
-  `npcbound_4_x` float NOT NULL,
-  `npcbound_4_y` float NOT NULL,
-  `npcbound_4_z` float NOT NULL,
+  `npclast_x` double NOT NULL,
+  `npclast_y` double NOT NULL,
+  `npclast_z` double NOT NULL,
+  `npcbound_1_x` double NOT NULL,
+  `npcbound_1_y` double NOT NULL,
+  `npcbound_1_z` double NOT NULL,
+  `npcbound_2_x` double NOT NULL,
+  `npcbound_2_y` double NOT NULL,
+  `npcbound_2_z` double NOT NULL,
+  `npcbound_3_x` double NOT NULL,
+  `npcbound_3_y` double NOT NULL,
+  `npcbound_3_z` double NOT NULL,
+  `npcbound_4_x` double NOT NULL,
+  `npcbound_4_y` double NOT NULL,
+  `npcbound_4_z` double NOT NULL,
   `npcalignment` int(11) NOT NULL COMMENT '0 = good 1 = evil 2 = chaotic 3 = neutral',
-  `npchome_x` float NOT NULL,
-  `npchome_y` float NOT NULL,
-  `npchome_z` float NOT NULL,
-  `npcsiminv_loc_x` float NOT NULL,
-  `npcsiminv_loc_y` float NOT NULL,
-  `npcsiminv_loc_z` float NOT NULL,
+  `npchome_x` double NOT NULL,
+  `npchome_y` double NOT NULL,
+  `npchome_z` double NOT NULL,
+  `npcsiminv_loc_x` double NOT NULL,
+  `npcsiminv_loc_y` double NOT NULL,
+  `npcsiminv_loc_z` double NOT NULL,
   `npcaiscript` varchar(15) NOT NULL,
   `npcskin` varchar(200) NOT NULL,
   `npcbirth` varchar(25) NOT NULL,
-  `npcbirth_loc_x` float NOT NULL,
-  `npcbirth_loc_y` float NOT NULL,
-  `npcbirth_loc_z` float NOT NULL,
+  `npcbirth_loc_x` double NOT NULL,
+  `npcbirth_loc_y` double NOT NULL,
+  `npcbirth_loc_z` double NOT NULL,
   `npcbuildmode` int(11) NOT NULL COMMENT '0 == off 1 == on',
   `npcbuildmodeldir` varchar(200) NOT NULL,
+  `npcrot` float NOT NULL,
+  `npcpitch` float NOT NULL,
+  `npcitminhand` int(11) NOT NULL,
   PRIMARY KEY (`npcid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- src/NPCManager.java
+++ src/NPCManager.java
@@ -2,11 +2,37 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.util.List;
 /**
  *
  * @author vijay
  */
 public class NPCManager {
+    public List<NPC> npcs;
+    /*
+     * Initalize a New NPC manager
+     * Witch will handel NPC adding and Removing as well
+     * as LoadBalancing of NPC Threads to keep CPU usage even
+     */
+    public NPCManager(){
+    
+    }
+    /*
+     * Add a NPC to Manage now this can be a new npc or an exsisting npc
+     */
+    public void AddNPCToManage(NPC npcToManage){
+        LoadBalance(npcToManage);
+    }
+    public void  LoadBalance(NPC npcToBalance){
 
+    }
+    public void KillThread(NPC npcThreadToKill){
+
+    }
+    public void RemoveNPCFromManagment(NPC npcToRemoveFromManagment){
+
+    }
+    public void ShiftNPCToCore(NPC npcToShift, int Core){
+
+    }
 }
